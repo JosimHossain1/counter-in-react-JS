47c70db6409a42de1cd9bf38815fb7542bd62a7d
Josim Hossain <emon.on321@gmail.com>
2022-05-26 12:58:30 +0600

    Fast react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Load></Load>
     </div>
   );
 }
+
+const Load = () => {
+  const [count, setCount] = useState(0);
+
+  const clicks = () => {
+    if(count < 1000){
+      setCount(count + 1);
+    }
+  };
+  const down = () => {
+    if (count > 0){
+      setCount(count - 1)
+    };
+  };
+
+  return (
+    <div className="box">
+      <div className="box1">
+        <h2>Total Balance: ${count} USD</h2>
+        <button className="btn" onClick={clicks}>Deposit</button>
+        <button className="btn" onClick={down}>Widthraw</button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
